b81adffbb8f77a052aef687dbc9433506ddb20f1
Victor Nguyen <victornguyen082@gmail.com>
2020-02-03 15:45:37 -0500

    created cart-item component and connected our cart dropdown with cartItem reducer

--- src/components/Cart-Dropdown.js
+++ src/components/Cart-Dropdown.js
@@ -1,17 +1,25 @@
 import React from 'react'
-
+import { connect } from 'react-redux'
 import CustomButton from './Custom-Button'
+import CartItem from './Cart-Item'
 
 import '../styles/cart-dropdown.scss'
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
 
     <div className="cart-dropdown">
         <div className="cart-items">
-
+            {
+                cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />
+                )
+            }
         </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
-export default CartDropdown
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown)
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -4,8 +4,8 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     if (existingCartItem) {
         return cartItems.map(cartItem =>
             cartItem.id === cartItemToAdd.id ?
-                { ...cartItem, quanity: cartItem.quanity + 1 } : cartItem)
+                { ...cartItem, quantity: cartItem.quantity + 1 } : cartItem)
     }
 
-    return [...cartItems, { ...cartItemToAdd, quanity: 1 }]
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 }
