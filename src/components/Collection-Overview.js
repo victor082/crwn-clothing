81407d6ae1335b03a89846c5b370c657274a323b
Victor Nguyen <victornguyen082@gmail.com>
2020-03-12 11:32:45 -0400

    move our directory and shop data into its own reducer, created respective collectors and updated corresponding components with new ux flow for directory/shop

--- src/components/Collection-Overview.js
+++ src/components/Collection-Overview.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { connect } from 'react-redux'
 import CollectionPreview from '../components/Collection-Preview'
 import { createStructuredSelector } from 'reselect'
-import { selectCollections } from '../redux/shop/shop-selectors'
+import { selectCollectionsForPreview } from '../redux/shop/shop-selectors'
 
 import '../styles/collection-overview.scss'
 
@@ -20,7 +20,7 @@ const CollectionOverview = ({ collections }) => {
 }
 
 const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
+    collections: selectCollectionsForPreview
 })
 
 export default connect(mapStateToProps)(CollectionOverview)
--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
 import CollectionOverview from '../components/Collection-Overview'
+import CollectionPage from './Collection-Page'
 
 const ShopPage = ({ match }) => {
 
     return (
         <div className="shop-page">
-            <Route exact path={'/shop'} component={CollectionOverview} />
+            <Route exact path={`${match.path}`} component={CollectionOverview} />
+            <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
         </div>
     )
 }
-
 
 
 export default ShopPage
